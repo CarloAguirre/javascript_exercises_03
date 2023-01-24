e01250eb5e962faa38a6f62dddee8cc08b434193
Carlo Aguirre <carlo_aguirre@outlook.cl>
2023-01-24 15:52:02 -0300

    img/src correction

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,55 +3,49 @@ const propiedadesJSON = [
       name: "Casa de campo",
       description: "Un lugar ideal para descansar de la ciudad",
       src:
-        "https://www.construyehogar.com/wp-content/uploads/2020/02/Dise%C3%B1o-casa-en-ladera.jpg",
+        "./assets/imgs/1.jpg",
       rooms: 2,
       m: 170,
-      img: "./assets/imgs/1.jpg"
     },
     {
       name: "Casa de playa",
       description: "Despierta tus días oyendo el oceano",
       src:
-        "https://media.chvnoticias.cl/2018/12/casas-en-la-playa-en-yucatan-2712.jpg",
+        "./assets/imgs/2.jpg",
       rooms: 2,
       m: 130,
-      img: "./assets/imgs/2.jpg"
     },
     {
       name: "Casa en el centro",
       description: "Ten cerca de ti todo lo que necesitas",
       src:
-        "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
+        "./assets/imgs/3.jpg",
       rooms: 1,
       m: 80,
-      img: "./assets/imgs/3.jpg"
     },
     {
       name: "Casa minimalista",
       description: "Conviertete en un nómada del mundo sin salir de tu casa",
       src:
-        "https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
+        "./assets/imgs/4.jpg",
       rooms: 1,
       m: 6,
-      img: "./assets/imgs/4.jpg"
     },
     {
       name: "Departamento",
       description: "Desde las alturas todo se ve mejor",
       src:
-        "https://www.adondevivir.com/noticias/wp-content/uploads/2016/08/depto-1024x546.jpg",
+        "./assets/imgs/5.jpg",
       rooms: 3,
       m: 200,
-      img: "./assets/imgs/5.jpg"
     },
     {
       name: "Mansión",
       description: "Vive una vida lujosa en la mansión de tus sueños ",
       src:
-        "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
+        "./assets/imgs/6.jpg",
       rooms: 5,
       m: 500,
-      img: "./assets/imgs/6.jpg"
     }
   ];
   
@@ -67,7 +61,7 @@ const propiedadesJSON = [
 
   for(let propiedad of propiedadesJSON){
     propiedadesContainer.innerHTML += `<div class="propiedad">
-                                        <div class="img" style="background-image: url(${propiedad.img})"></div>
+                                        <div class="img" style="background-image: url(${propiedad.src})"></div>
                                         <section>
                                             <h5>${propiedad.name}</h5>
                                             <div class="d-flex justify-content-between">
@@ -96,7 +90,7 @@ let onClickForm = ()=>{
       if(propiedad.rooms === Number(roomsNumber.value) && propiedad.m >= Number(minMetros.value) && propiedad.m <= Number(maxMetros.value)){
         nuevoTotalPropiedades +=1;
         propiedadesContainer.innerHTML += `<div class="propiedad">
-                                            <div class="img" style="background-image: url(${propiedad.img})"></div>
+                                            <div class="img" style="background-image: url(${propiedad.src})"></div>
                                             <section>
                                                 <h5>${propiedad.name}</h5>
                                                 <div class="d-flex justify-content-between">
